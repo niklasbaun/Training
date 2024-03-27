a45751f0df94fcae3ddaa617f3ca4c4f7940507f
niklas <niklasbaun@gmail.com>
2024-03-27 14:56:45 +0100

    first chapter solutions for training exercises

---
+++ Kap_02/src/ex2_2.java
@@ -0,0 +1,18 @@
+public class ex2_2 {
+    //a)
+    private int predecessor(int n) {
+        return n - 1;
+    }
+
+    //b)
+    private boolean isEqual(String a, String b){
+        return a.equals(b);
+    }
+
+    //public static void main(String[] args) {
+    //    ex2_2 ex = new ex2_2();
+    //    System.out.println(ex.predecessor(5));
+    //    System.out.println(ex.isEqual("Hello", "Hello"));
+    //}
+
+}
